97d8996c9dc7284ebe28d7b868f0323b29e702bf
tanglijun <tanglijun@YLMF-2019VGWXPS>
2019-07-15 22:53:03 +0800

    修改88888cccc

--- src/main/java/com/gu/study/Test.java
+++ src/main/java/com/gu/study/Test.java
@@ -7,11 +7,10 @@ public class Test {
 
         System.out.println("2222");
         System.out.println("dev2");
-        System.out.println("44444");
-        System.out.println("55555");
+        System.out.println("444441");
+        System.out.println("555551");
 
         System.out.println("666");
-        System.out.println("777");
 
         System.out.println("777");
     }
